1793bb05fbf2bde4165f54bf412bfa882b382610
Akhil Verma <70507574+akhilvofficial9@users.noreply.github.com>
2020-10-28 11:01:16 +0530

    Add files via upload

---
+++ php-var.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $txt ="Hello World!"; //when you assign text value always put quotes around the value
+    $x = 5;
+    $y = 10.5 ;
+    echo $txt;
+    echo "<br>";
+    echo $x;
+    echo "<br>";
+    echo $y;
+    ?>
+    
+
+    <?php
+    $txt2 = "Akhil Verma";
+    echo "<br>" . "My name is " . $txt2 . "!";
+    ?>
+</body>
+</html>
