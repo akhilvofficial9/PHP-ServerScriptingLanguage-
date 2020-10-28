05cbbcf50810f9d55fa5587958f7e0cf13d86e6b
Akhil Verma <70507574+akhilvofficial9@users.noreply.github.com>
2020-10-28 10:25:41 +0530

    Add files via upload

---
+++ example.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+
+
+<body>
+<?php
+$color = "one";//Three different variables with 3 different values
+$COLOR = "two";
+$coLOR = "three";
+echo "My car is " . $color . "<br>"; 
+echo "My house is " . $COLOR . "<br>";
+echo "My boat is " . $coLOR . "<br>";
+?> 
+<h1>Heading</h1>
+<?php
+echo "this is my first php script!";
+?>    
+</body>
+</html>
+<?php
+EChO "this is my first php script!";
+ ?>
