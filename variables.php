1b41649760500cddfbea680a896009de7863f0c8
Akhil Verma <70507574+akhilvofficial9@users.noreply.github.com>
2020-10-29 16:51:16 +0530

    Add files via upload

---
+++ variables.php
@@ -0,0 +1,23 @@
+<?php
+$name ="Php";
+var_dump($name);
+echo "<br>";
+$number = 10;
+var_dump($number);
+echo "<br>";
+$number = 10838;
+var_dump($number);
+echo "<br>";
+$number = 1.30;
+var_dump($number);
+echo "<br>";
+$number = true;
+var_dump($number);
+echo "<br>";
+echo "<br>This is PHP","<br>This is another line";
+print "<br>This is PHP"."<br>This is another line";
+$num1=100;
+$num2=200;
+echo "<br>Sum of two numbers is:", ($num1+$num2);
+print "<br>Sum of two numbers is:".($num1+$num2);
+?>
