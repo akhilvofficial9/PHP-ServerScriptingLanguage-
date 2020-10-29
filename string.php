a5b354d8768d5151ef19c89d4e826cca66f500bb
Akhil Verma <70507574+akhilvofficial9@users.noreply.github.com>
2020-10-29 19:11:50 +0530

    Add files via upload

---
+++ string.php
@@ -0,0 +1,18 @@
+<?php
+echo strlen("Hello World!");
+?>
+
+<?php
+echo "<br>";
+echo str_word_count("Hello World!");
+?>
+
+<?php
+echo "<br>";
+echo strrev("Hello World!");
+?>
+
+<?php
+echo "<br>";
+echo strpos("Hello world!","world");
+?>
