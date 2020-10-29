7e83fc3af84a751fa2891ed81d47e03fb6cbba23
Akhil Verma <70507574+akhilvofficial9@users.noreply.github.com>
2020-10-29 15:20:13 +0530

    Add files via upload

---
+++ datatypes.php
@@ -0,0 +1,21 @@
+<?php
+$x = "Hello World!";//This is a string
+echo $x;
+echo "<br>";
+?>
+
+
+<?php
+$x = 5949; //this is an integer
+var_dump($x); // PHP var_dump() function returns the datatype and value. 
+?>
+
+<?php
+$x = 54.9; //this is an float
+var_dump($x); // PHP var_dump() function returns the datatype and value. 
+?>
+
+<?php
+$cars = array("Volvo","BMW","Toyota");
+var_dump($cars);
+?>
---
+++ echo print.php
@@ -0,0 +1,13 @@
+<?php
+$x = 5;
+$y = 10;
+echo "<h2> PHP is fun <br>";
+echo "This" , "is" , "a" , "string";
+echo "<br>";
+echo $x + $y;
+
+print "<h2> PHP is fun <br>";
+print "This" . "is" . "a" . "string";
+print "<br>";
+print $x + $y;
+?>
